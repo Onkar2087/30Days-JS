f30abd67b53f6e23cca52c3e4308817cc6889515
Onkar2087 <114167896+Onkar2087@users.noreply.github.com>
2024-07-14 00:54:52 +0530

    Add files via upload

---
+++ Day 1/FeatureRequest.js
@@ -0,0 +1,24 @@
+//1
+let v3 = 1;
+let v4 = "Samosa";
+let v5 = false;
+let v6 = {
+    1:"Lunes",
+    7:"Domingo"
+}
+let v7 = [1, "Hola", false];
+
+console.log(typeof(v3));
+console.log(typeof(v4));
+console.log(typeof(v5));
+console.log(typeof(v6));
+console.log(typeof(v7));
+
+//2
+let x = 78;
+x = v3;
+console.log(x);
+
+const y = 99;
+y = v3;
+console.log(y);
---
+++ Day 1/Tasks.js
@@ -0,0 +1,38 @@
+//Task-1
+var v1 = 5;
+console.log(v1);
+
+//Task-2
+let s1 = "JS";
+console.log(s1);
+
+//Task-3
+const v2 = false;
+console.log(v2);
+
+//Task-4
+let v3 = 1;
+let v4 = "Samosa";
+let v5 = false;
+let v6 = {
+    1:"Lunes",
+    7:"Domingo"
+}
+let v7 = [1, "Hola", false];
+
+console.log(typeof(v3));
+console.log(typeof(v4));
+console.log(typeof(v5));
+console.log(typeof(v6));
+console.log(typeof(v7));
+
+//Task-5
+let v8 = 9;
+console.log(v8);
+v8 = v3;
+console.log(v8);
+
+//Task-6
+const v9 = 5;
+// v9 = 77;
+console.log(v9);
