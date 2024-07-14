bee8f761df1d78e02827764f43fb99007577de6f
Onkar2087 <114167896+Onkar2087@users.noreply.github.com>
2024-07-15 01:04:24 +0530

    Add files via upload

---
+++ Day 2/FeatureRequest.js
@@ -0,0 +1,20 @@
+//1
+let n1 = 31;
+let n2 = 20;
+
+console.log(`Addition Result of ${n1} and ${n2} is ${n1 + n2}`);
+console.log(`Subtraction Result of ${n1} and ${n2} is ${n1 - n2}`);
+console.log(`Multiplication Result of ${n1} and ${n2} is ${n1 * n2}`);
+console.log(`Division Result of ${n1} and ${n2} is ${n1 / n2}`);
+console.log(`Remainder Result of ${n1} and ${n2} is ${n1 % n2}`);
+
+//2
+console.log(n1>n2);
+console.log(n1<n2);
+console.log(n1 === n2);
+console.log(n1 != n2);
+
+
+//3
+let s = n1>0 ? "Positive" : "Negative"
+console.log(s);
---
+++ Day 2/Tasks.js
@@ -0,0 +1,61 @@
+//Task-1
+let n1 = 5;
+let n2 = 9;
+console.log(`The sum of the 2 numbers is ${n1 + n2}`);
+
+//Task-2
+console.log(`The difference of the 2 numbers is ${n1 - n2}`);
+
+//Task-3
+console.log(`The product of the 2 numbers is ${n1 * n2}`);
+
+//Task-4
+console.log(`The division of the 2 numbers is ${n1 / n2}`);
+
+// Task-5
+console.log(`The remainder of the 2 numbers is ${n1 % n2}`);
+
+// Task-6
+let x = 56;
+x += 90;
+console.log(x);
+
+//Task-7
+let y = 78;
+y -= 67;
+console.log(y);
+
+//Task-8
+let num1 = 56;
+let num2 = 34;
+console.log(num1 > num2);
+console.log(num1 < num2);
+
+//Task-9
+let i = 1;
+let j = 2;
+console.log(i >= j);
+console.log(i <= j);
+
+//Task-10
+let k1 = "4";
+let k2 = 4;
+console.log(k1 == k2);
+console.log(k1 === k2);
+
+//Task-11
+let c1 = (5<7) && (2>3)
+console.log(c1);
+
+//Task-12
+let c2 = (5<7) || (2>3)
+console.log(c2);
+
+// Task-13
+let c3 = !(5==="5")
+console.log(c3);
+
+// Task-14
+let c4 = (45%2 == 0) ? "Even" : "Odd";
+console.log(c4);
+
