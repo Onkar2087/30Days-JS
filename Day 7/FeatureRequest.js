8d12934d89d4b7b2cc969c6db321f0d8b4133516
Onkar2087 <114167896+Onkar2087@users.noreply.github.com>
2024-07-21 01:28:32 +0530

    Update FeatureRequest.js

--- Day 7/FeatureRequest.js
+++ Day 7/FeatureRequest.js
@@ -4,11 +4,43 @@ book = {
     title : "Genz Youtuber",
     author : "Steeper Boy",
     year : 2017,
-    bookInfo : () => console.log(`Title of the book is ${this.title} and Author of the book is ${this.author}`),
+    bookInfo : function() {console.log(`Title of the book is ${this.title} and Author of the book is ${this.author}`)},
     updateYear : (saal) => this.year = saal,
-    ret : () => console.log(`Title of the book is ${this.title} and year is ${this.year}`)
+    ret : function() {console.log(`Title of the book is ${this.title} and year is ${this.year}`)}
 }
 console.log(book.title);
 book.ret();
 book.bookInfo();
 console.log("\n");
+
+//2
+console.log("Feature Request - 2");
+library = {
+    name : "Aaj ki Library",
+    books : [
+        {
+            title : "Champak",
+            author : "Hoga koi",
+            year : 2005
+        },
+        {
+            title : "Tinkle",
+            author : "No idea",
+            year : 2007
+        }
+    ]
+}
+console.log(library);
+console.log("\n");
+
+//3
+console.log("Feature Request - 3");
+for (const key in book) {
+    console.log(`Property = ${key}`);
+    console.log(`Value = ${book[key]}`);
+    console.log("\n");
+}
+
+console.log(Object.keys(Book));
+console.log("\n");
+console.log(Object.values(book));
